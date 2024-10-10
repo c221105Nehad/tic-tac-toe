ef8a29fddeb223a703039505558f2ba16cc787ad
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-10-10 09:52:42 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def printBoard():
+    print(f'0 | 1 | 2');
+    print(f'--|---|--');
+    print(f'3 | 4 | 5');
+    print(f'--|---|--');
+    print(f'6 | 7 | 8');
+
+if __name__ == "__main__":
+    xState = [0, 0, 0, 0, 0, 0, 0];
+    zState = [0, 0, 0, 0, 0, 0, 0];
+    turn = 1;
+    print('welcome to tic tac toe');
+    while true:
+        print('hello');
+        
